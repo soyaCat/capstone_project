b726d49256e9b104e16cc81a27d4a2ea1b580c6f
soya <soyamilk432@gmail.com>
2021-06-01 02:32:22 +0900

    오차 수정(범위, 파란색)

--- capstone_project/image_function.py
+++ capstone_project/image_function.py
@@ -12,12 +12,12 @@ real_green_minc = np.array([65, 165, 85])
 real_green_maxc = np.array([85, 195, 117])
 '''
 
-real_red_minc = np.array([185, 30, 44])
+real_red_minc = np.array([170, 30, 44])
 real_red_maxc = np.array([220, 90, 110])
-real_blue_minc = np.array([18, 76, 230])
+real_blue_minc = np.array([5, 50, 215])
 real_blue_maxc = np.array([72, 130, 255])
-real_green_minc = np.array([33, 130, 108])
-real_green_maxc = np.array([60, 160, 130])
+real_green_minc = np.array([25, 120, 100])
+real_green_maxc = np.array([65, 160, 150])
 
 fake_red = np.array([161, 0, 0])
 fake_orange = np.array([255, 75, 0])
@@ -26,7 +26,7 @@ fake_green = np.array([0, 113, 15])
 fake_yellow = np.array([192, 190, 0])
 fake_pulple = np.array([108, 0, 97])
 
-roi_parameter = [142,22,300,250]
+roi_parameter = [68,22,300,250]
 
 count_y_green = 0
 seta_H = 0
@@ -287,9 +287,9 @@ def image_process(image):
     seta = 0
     roi = image[y:y + h, x:x + w]
     roi = np.array(roi)
-    arr = cv2.GaussianBlur(roi, (3, 3), 0)
-    plt.imshow(arr)
+    plt.imshow(roi)
     plt.show()
+    arr = cv2.GaussianBlur(roi, (3, 3), 0)
     list2 = slide_real_image(arr)
     list3 = input_image()
     for index, i in enumerate(list2):
--- capstone_project/image_test.py
+++ capstone_project/image_test.py
@@ -2,9 +2,9 @@ import image_function as IMG_F
 import cv2
 import matplotlib.pyplot as plt
 
-for i in range(37, 41):
-    img = cv2.imread("./test_real_img/"+str(i)+".jpg")
-    img = cv2.resize(img, (588, 286), interpolation=cv2.INTER_LANCZOS4)
+for i in range(1,4):
+    img = cv2.imread("./real2_img/"+str(i)+".jpg")
+    img = cv2.resize(img, (440, 290), interpolation=cv2.INTER_LANCZOS4)
     img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
     result, seta = IMG_F.image_process(img)
     plt.imshow(result)
